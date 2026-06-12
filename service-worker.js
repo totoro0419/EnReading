28031d3bb1d16e194fec752d7f8ce873fb49609f
totoro0419 <0419totoromori@gmail.com>
2026-06-12 18:44:02 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = 'sne-reader-dict-pos-usage-v1-9-0';
+const ASSETS = ['./', './index.html', './manifest.json', './icon.png'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME && k.startsWith('sne-reader')).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).catch(() => caches.match('./index.html'))));
+});
